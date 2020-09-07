40008bd935394723c0a6e24aafc97f34b58ee7ed
Brian Blais <bblais@gmail.com>
2020-09-07 15:06:27 -0400

    more misc

--- 2020-09-07 - Misc.py
+++ 2020-09-07 - Misc.py
@@ -169,8 +169,81 @@ b
 b.show_locations()
 
 
+# In[31]:
+
+
+board=Board(3,5)
+board
+
+
+# In[33]:
+
+
+for i in range(15):
+    board[i]=1
+
+
+# In[34]:
+
+
+board
+
+
+# In[36]:
+
+
+board=Board(3,3)
+board[0,1]=1
+board[1,0]=1
+board[2,1]=2
+board[2,2]=2
+board
+
+
+# In[37]:
+
+
+board.show_locations()
+
+
+# In[40]:
+
+
+state=board
+player=1
+
+
+# In[41]:
+
+
+moves=[]
+if player==1:
+    if state[0]==1 and state[1]==0:
+        moves.append( [0,1])
+    
+else:
+    pass
+
+
+# In[42]:
+
+
+board=Board(5,6)
+board.show_locations()
+
+
 # In[ ]:
 
 
+def check_three_in_a_row(state,player,i1,i2,i3):
+    if state[i1]==player and state[i2]==player and state[i3]==player:
+        return True
+    else:
+        return False
+    
 
+# horiz
+for loc in [0,1,2,3,6,7,8,12,13,14,15]:
+    if check_three_in_a_row(state,player,loc,loc+1,loc+2):
+        return "win"
 
