160c2eaa513097c199c26cf67dc35aafe038fe59
Brian Blais <bblais@gmail.com>
2020-10-05 15:10:03 -0400

    class notebook update

--- 2020-10-05 - Robot Sim Stuff.py
+++ 2020-10-05 - Robot Sim Stuff.py
@@ -192,7 +192,7 @@ plot(t,d)
 # 3. stop
 # 
 
-# In[47]:
+# In[49]:
 
 
 def turn_a_bit(t,robot):
@@ -256,7 +256,7 @@ robot.last_distance=500
 run_sim(env,turn_90_degrees,
         total_time=5,  # seconds
         dt=1/60,
-        dt_display=.2,  # make this larger for a faster display
+        dt_display=.05,  # make this larger for a faster display
         figure_width=8,
         plot_orientation=False,
        )    
@@ -267,6 +267,220 @@ run_sim(env,turn_90_degrees,
 
 t,d=robot.storage.arrays()
 plot(t,d)
+
+
+# In[50]:
+
+
+def turn_a_bit(t,robot):
+    
+    robot.message = 'turn'
+    if t<0.9:
+        robot['left'].F=-10
+        robot['right'].F=10
+        return False        # not done yet!
+    else:
+        return True        #  done with this behavior
+    
+    
+def turn_to_min_distance(t,robot):
+    robot.message = 'min distance'
+    
+    d=robot['left'].read_distance()
+    if d<robot.last_distance:
+        robot['left'].F=-10
+        robot['right'].F=10
+        robot.last_distance=d
+        return False
+    else:
+        return True
+    
+def stop(t,robot):
+    robot.message = 'stop'
+    robot['left'].F=0
+    robot['right'].F=0
+    
+    return False  # never done
+    
+
+def turn_90_degrees(t,robot):
+    
+    actions=[turn_a_bit,turn_to_min_distance,stop]
+    
+    if t==0.0:  # first time
+        robot.current_action=0
+        robot.start_time=0.0
+        
+    action=actions[robot.current_action]
+    
+    value=action(t-robot.start_time,robot)
+    
+    if value:  # done with this action
+        robot.current_action+=1
+        robot.start_time=t
+        
+
+    robot.storage+=t,robot['left'].read_distance()
+
+    
+env=Environment(image='../images/linepath1.jpeg', # up one folder example
+                linearDamping=20) 
+robot=Robot(env)
+
+build(robot)
+robot.storage=Storage()
+robot.last_distance=500
+
+run_sim(env,turn_90_degrees,
+        total_time=5,  # seconds
+        dt=1/60,
+        dt_display=.05,  # make this larger for a faster display
+        figure_width=8,
+        plot_orientation=False,
+       )    
+
+
+# # Many-part robot
+
+# In[60]:
+
+
+def build_with_funny_bug(robot):    
+    
+    # use loops!
+    w=.3
+    buffer=0.05
+    x=2
+    y=10
+    
+    boxes=[]
+    old_b=None
+    
+    for i in range(10):
+        new_b=Box(robot,x,y+(w+buffer)*i,width=w,height=w,
+            name='box%d' % i)
+        boxes.append(new_b)
+        
+        if not old_b is None:
+            connect(old_b,new_b,'weld')
+        
+        old_b=new_b
+        
+
+
+# In[70]:
+
+
+def build(robot):    
+    
+    # use loops!
+    w=.3
+    buffer=0.05
+    x=2
+    y=10
+    
+    boxes=[]
+    for i in range(10):
+        new_b=Box(robot,x,y+(w+buffer)*i,width=w,height=w,
+            name='box%d' % i)
+        boxes.append(new_b)
+        
+
+    for i,b1 in enumerate(boxes):
+        for j,b2 in enumerate(boxes[i+1:]):
+            connect(b1,b2,'weld')
+
+
+# In[71]:
+
+
+def act(t,robot):
+    robot['box0'].F=10
+    robot['box9'].F=10
+
+
+# In[85]:
+
+
+def turn_a_bit(t,robot):
+    
+    robot.message = 'turn'
+    if t<0.3:
+        robot['box9'].F=-10
+        robot['box0'].F=10
+        return False        # not done yet!
+    else:
+        return True        #  done with this behavior
+    
+    
+def turn_to_min_distance(t,robot):
+    robot.message = 'min distance'
+    
+    d=robot['box9'].read_distance()
+    if d<=robot.last_distance:
+        robot['box9'].F=-2
+        robot['box0'].F=2
+        robot.last_distance=d
+        return False
+    else:
+        return True
+    
+def stop(t,robot):
+    robot.message = 'stop'
+    robot['box9'].F=0
+    robot['box0'].F=0
+    
+    return False  # never done
+    
+
+def turn_90_degrees(t,robot):
+    
+    actions=[turn_a_bit,turn_to_min_distance,stop]
+    
+    if t==0.0:  # first time
+        robot.current_action=0
+        robot.start_time=0.0
+        
+    action=actions[robot.current_action]
+    
+    value=action(t-robot.start_time,robot)
+    
+    if value:  # done with this action
+        robot.current_action+=1
+        robot.start_time=t
+        
+
+    robot.storage+=t,robot['box0'].read_distance()
+
+    
+env=Environment(image='../images/linepath1.jpeg', # up one folder example
+                linearDamping=20) 
+robot=Robot(env)
+
+build(robot)
+robot.storage=Storage()
+robot.last_distance=500
+
+run_sim(env,turn_90_degrees,
+        total_time=5,  # seconds
+        dt=1/60,
+        dt_display=.1,  # make this larger for a faster display
+        figure_width=8,
+        plot_orientation=False,
+       )    
+
+
+# In[86]:
+
+
+t,d=robot.storage.arrays()
+plot(t,d)
+
+
+# In[87]:
+
+
+d
 
 
 # In[ ]:
