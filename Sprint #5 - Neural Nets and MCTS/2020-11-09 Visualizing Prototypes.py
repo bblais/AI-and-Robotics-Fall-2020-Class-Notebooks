394dbbdc1fe23e2cbbb47cecb9512a40e5697d61
Brian Blais <bblais@gmail.com>
2020-11-09 15:12:34 -0500

    added some board pictures to use as an example for prototypes

--- Sprint #5 - Neural Nets and MCTS/2020-11-09 Visualizing Prototypes.py
+++ Sprint #5 - Neural Nets and MCTS/2020-11-09 Visualizing Prototypes.py
@@ -91,6 +91,84 @@ p = C.means[1].reshape(20,10,3)
 imshow(p)
 
 
+# In[45]:
+
+
+images=image.load_images('armen images/')
+
+
+# In[46]:
+
+
+data=image.images_to_vectors(images)
+data.vectors/=255
+
+
+# In[47]:
+
+
+data_train,data_test=split(data,test_size=0.2)
+
+
+# In[48]:
+
+
+C=NaiveBayes()
+C.fit(data_train.vectors,data_train.targets)
+print("On Training Set:",C.percent_correct(data_train.vectors,data_train.targets))
+print("On Test Set:",C.percent_correct(data_test.vectors,data_test.targets))
+
+
+# # Prototypes
+# 
+# These prototypes look all the same....why?
+
+# In[49]:
+
+
+figure(figsize=(12,6))
+p={}
+for i in range(3):
+    subplot(1,3,i+1)
+    p[i] = C.means[i].reshape(data.shape)
+    imshow(p[i])
+    title(data.target_names[i])
+
+
+# In[51]:
+
+
+data.target_names
+
+
+# In[53]:
+
+
+piece1=0
+piece2=2
+blank=1
+
+
+# look at differences -- piece 2 minus blank, and piece 1 minus blank, rescaled to fill the color space.
+
+# In[54]:
+
+
+d=p[piece1]-p[blank]
+d=d-d.min()
+d=d/d.max()
+imshow(d)
+
+
+# In[55]:
+
+
+d=p[piece2]-p[blank]
+d=d-d.min()
+d=d/d.max()
+imshow(d)
+
+
 # In[ ]:
 
 
